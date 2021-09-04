58674cfcdc003e29860db689ef5b8ac25003b436
E-Robin <grobin578@gmail.com>
2021-09-05 00:16:40 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,38 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile("<h1>Hello</h1>");
+});
+
+
+//api endpoint
+
+
+app.get("/api/:text" , function (req ,res)
+{
+  //const d = req.params.text ;
+
+
+ 
+        res.json({name: "api" ,content : "Yoda"}) ;
+    
+         
+
+})
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("http://localhost:"+listener.address().port) ;
+});
